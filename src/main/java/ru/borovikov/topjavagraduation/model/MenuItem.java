bdf0f09bcfd3ab788184082f789ce1da4d0169fa
Ekyd9 <ekyd777@gmail.com>
2025-11-16 21:21:32 +0300

    added menu items

---
+++ src/main/java/ru/borovikov/topjavagraduation/model/MenuItem.java
@@ -0,0 +1,33 @@
+package ru.borovikov.topjavagraduation.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "menu_items", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"menu_id", "dish_id"}, name = "uk_menu_items_menu_dish")
+})
+public class MenuItem extends AbstractBaseEntity {
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dish_id", nullable = false)
+    private Dish dish;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    @NotNull
+    @DecimalMin(value = "0.01", message = "Price must be positive")
+    private BigDecimal price;
+}
